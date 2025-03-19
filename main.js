6318f53e2a934299f72327710b8fec660a24ff11
Juan <content@whitehat4u.com>
2025-03-19 22:05:19 +0100

    WebGL support

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -22,5 +24,12 @@ function animate() {
 
 
 
-renderer.setAnimationLoop(animate);
+if (WebGL.isWebGLAvailable()) {
+  renderer.setAnimationLoop(animate);
+} else {
+  const warning = WEBGL.getWebGLErrorMessage();
+  document.getElementById('container').appendChild(warning);
+}
+
+
 
